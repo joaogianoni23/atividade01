9b14eca4b3e48f58fb2ded96230219af35348831
Joao Gianoni <joaogianoni.tds2.senai@gmail.com>
2024-10-03 09:02:29 -0300

    refactor: alter server to use routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,16 +1,11 @@
 import { Router } from "express";
 
-import emocoesRoutes from "./emocoes.routes.js";
-import personagensRoutes from "./personagens.routes.js";
+import candidatosRoutes from "./candidatos.routes.js";
 
 const routes = Router();
 
 routes.get("/", (req,res) => {
     return res.status(200).send({message:"Vai Corinthians!"})
-});
-
-routes.get("/2tds2", (req, res) => {
-    return res .status(200).send({ message: "Hello World!"})
 });
 
 routes.use("/candidatos", candidatosRoutes);
--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
-import express from "express";
-import { config } from "dotenv";
+import express from 'express'
+import { config } from 'dotenv'
 
-config();
+import routes from './routes/index.routes.js';
 
-const serverPort = process.env.PORT || 4000;
+config()
 
-const app = express();
-app.use(express.json());
+const serverPort = process.env.PORT || 4000
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, World!" });
-});
+const app = express()
+app.use(express.json())
+app.use(routes);
 
 app.listen(serverPort, () => {
-  console.log(`🤑 Server started on http://localhost:${serverPort}`);
-});
+    console.log(`🤑 Server started on http://localhost:${serverPort}`)
+})
